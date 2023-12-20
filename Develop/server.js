caee1fff4ae4a1c8daa77db4046b948f46747fed
unknown <attaway.code@gmail.com>
2023-12-20 13:36:10 -0800

    adds README and more comments

--- Develop/server.js
+++ Develop/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-
-// Helper function for generating unique ids
+// process.env.PORT is used for vload hosting the server from Heroku
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// "Mounting" the routes from api onto the object: app
 app.use('/api', api);
 
 app.use(express.static('public'));
-
 
 // GET /notes should return the notes.html file.
 app.get('/notes', (req, res) =>
@@ -26,6 +26,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
+// displays the port which the application is running on 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
